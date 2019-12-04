37651999d04916f1588754a90ffa97ca723b424c
_sengar <51945068+sengar1729@users.noreply.github.com>
2019-12-04 21:26:39 +0530

    Update Linear_Search.c

--- Searching/Linear_Search.c
+++ Searching/Linear_Search.c
@@ -1,1 +1,35 @@
-
+#include<stdio.h>
+int linear_search(int [],int,int);
+void main()
+{
+    int n,i,arr[20],s;
+    printf("Enter Size of Array: ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter the no. you want to search: ");
+    scanf("%d",&s);
+    i=linear_search(arr,n,s);
+    if(i>0)
+    {
+        printf("No. found on %dth Location\n",i+1);
+    }
+    else
+    {
+        printf("N0. Not Found\n");
+    }
+}
+int linear_search(int arr[],int n,int s)
+{
+    int i=-1;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==s)
+        {
+            break;
+        }
+    }
+    return i;
+}
